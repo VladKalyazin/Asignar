dde18c541549352b957f4529568a76eedf8959e0
VladKalyazin <999vlad@gmail.com>
2016-06-24 02:46:42 +0300

    authorization

--- BugsTrackingSystem/BugsTrackingSystem/Scripts/scripts.js
+++ BugsTrackingSystem/BugsTrackingSystem/Scripts/scripts.js
@@ -1,10 +1,4 @@
-﻿$(document).ready(function () {
-    $("#searchTool").click(function () {
-        $("#searchText").fadeToggle(500);
-    });
-});
-
-$(document).ready(function ($) {
+﻿$(document).ready(function ($) {
     $(".clickable-row").click(function () {
         window.document.location = $(this).data("href");
     });
---
+++ BugsTrackingSystem/BusinessLogic/Account/UserAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AsignarDBEntities;
+
+namespace BusinessLogic.Account
+{
+    public class UserAction : IDisposable
+    {
+        private readonly AsignarDatabaseModel _databaseModel = new AsignarDatabaseModel();
+
+        public bool ValidateUser(string email, string password)
+        {
+            bool isValid = false;
+
+            var currentUser = (from user in _databaseModel.Users where user.Email == email && user.Password == password select user).FirstOrDefault();
+
+            if (currentUser != null)
+                isValid = true;
+
+            return isValid;
+        }
+
+        public void Dispose()
+        {
+            _databaseModel.Dispose();
+        }
+    }
+}
