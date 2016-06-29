c1eefe0fff87d97c12cb50f1c85eaefa1a5e638a
Oleksandra Tkalich <oleksandratk@gmail.com>
2016-06-30 01:44:12 +0300

    Changed pages, added popups, etc

--- BugsTrackingSystem/BugsTrackingSystem/Scripts/scripts.js
+++ BugsTrackingSystem/BugsTrackingSystem/Scripts/scripts.js
@@ -28,3 +28,28 @@ $("#opener").click(function () {
     $("#add_project").css({ 'display': "inline" });
     $("#add_project").dialog("open");
 });
+
+$(function () {
+    $("#add_task").dialog
+    ({
+        modal: true,
+        width: 600,
+        height: 300,
+        draggable: true,
+        resizable: true,
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 500
+        },
+        hide: {
+            effect: "puff",
+            duration: 500
+        }
+    });
+});
+
+$(".open").click(function () {
+    $("#add_task").css({ 'display': "inline" });
+    $("#add_task").dialog("open");
+});
