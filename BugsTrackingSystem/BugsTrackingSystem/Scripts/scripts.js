e93bc8a038078c0bbc210f567d49404b93b813f2
oleksandratk <oleksandratk@gmail.com>
2016-07-06 02:08:48 +0300

    lots of changes on different pages

--- BugsTrackingSystem/BugsTrackingSystem/Scripts/scripts.js
+++ BugsTrackingSystem/BugsTrackingSystem/Scripts/scripts.js
@@ -37,29 +37,35 @@ $("#opener").click(function () {
     $("#add_project").dialog("open");
 });
 
-//$(function () {
-//    $("#add_user").dialog
-//    ({
-//        modal: true,
-//        width: 600,
-//        height: 300,
-//        draggable: true,
-//        resizable: false,
-//        autoOpen: false,
-//        show: {
-//            effect: "blind",
-//            duration: 500
-//        },
-//        hide: {
-//            effect: "puff",
-//            duration: 500
-//        }
-//    });
-//    $(".ui-dialog-title").html("<i class=\"fa fa-users close_popup\" aria-hidden=\"true\"><span class=\"\"> Create new user</span></i>");
-//    $(".ui-dialog-titlebar-close").append("<i class=\"fa fa-times close_popup\" aria-hidden=\"true\"></i>");
-//});
-
 $("#open_user").click(function () {
     $("#add_user").css({ 'display': "inline" });
     $("#add_user").dialog("open");
+});
+
+
+$(function () {
+    $("#edit_project").dialog
+    ({
+        modal: true,
+        width: 600,
+        height: 300,
+        draggable: true,
+        resizable: false,
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 500
+        },
+        hide: {
+            effect: "blind",
+            duration: 500
+        }
+    });
+    $(".ui-dialog-title").html("<i class=\"fa fa-list close_popup\" aria-hidden=\"true\"><span class=\"\"> Edit project</span></i>");
+    $(".ui-dialog-titlebar-close").append("<i class=\"fa fa-times close_popup\" aria-hidden=\"true\"></i>");
+});
+
+$("#open_edit").click(function () {
+    $("#edit_project").css({ 'display': "inline" });
+    $("#edit_project").dialog("open");
 });
